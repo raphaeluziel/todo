4296504ddbcf01e7cdfdc5b68bade893f81f0efe
Raphael Uziel <raphaeluziel@hotmail.com>
2019-12-25 18:38:51 -0500

    think it works!

--- script.js
+++ script.js
@@ -9,70 +9,83 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-let checkedItems = 0
-
 function newTodo() {
 
   // Ask user for a new todo item by sending out a prompt
   let item = prompt("What should be added to the todo list?")
 
+  // Check how many items and unchecked ones at start
   let numItems = itemCountSpan.innerHTML
+  let uncheckedItems = uncheckedCountSpan.innerHTML
 
   // Only if user writes a new todo, add it to the todo list
   if (item){
 
+    // There is now a new item, which is not checked
     numItems++
-    uncheckedCountSpan.innerHTML = numItems
+    uncheckedItems++
 
+    // Create a checkbox for the new todo item
     let checkBox = document.createElement('input')
     checkBox.type = 'checkbox'
     checkBox.className = classNames.TODO_CHECKBOX
     checkBox.id = 'checkbox' + numItems
+
+    // When check box is clicked update the counts shown
     checkBox.addEventListener('click', function(){
       if (checkBox.checked){
-        checkedItems = parseInt(uncheckedCountSpan.innerHTML) + 1
+        uncheckedItems = parseInt(uncheckedCountSpan.innerHTML) - 1
       }
       else{
-        checkedItems = parseInt(uncheckedCountSpan.innerHTML) - 1
+        uncheckedItems = parseInt(uncheckedCountSpan.innerHTML) + 1
       }
-      uncheckedCountSpan.innerHTML = checkedItems
+      uncheckedCountSpan.innerHTML = uncheckedItems
     })
 
+    // Create a label for this checkbox (the actual todo item)
     let checkBoxLabel = document.createElement('label')
     checkBoxLabel.htmlFor = 'checkbox' + numItems
     checkBoxLabel.className = classNames.TODO_TEXT
-
     let textNode = document.createTextNode(item)
-
     checkBoxLabel.appendChild(textNode)
 
+    // Create a button to delete the item (and itself)
+    const button = document.createElement('button')
+    button.innerHTML = 'delete'
+    button.className = classNames.TODO_DELETE + 'btn btn-danger btn-sm'
+
+    button.addEventListener('click', function(){
+      // Check how many items and unchecked first
+      numItems = itemCountSpan.innerHTML
+      uncheckedItems = uncheckedCountSpan.innerHTML
+
+      // If the item to be deleted was not checked, decrease number
+      // of items that are unchecked
+      if (!document.getElementById('checkbox' + numItems).checked){
+        console.log("removing unchecked")
+        uncheckedItems--
+      }
+      // When button is clicked, delete the whole item, and update counts
+      document.getElementById('listItem' + numItems).remove()
+      console.log("removing anything")
+      numItems--
+
+      // Update the view
+      itemCountSpan.innerHTML = numItems
+      uncheckedCountSpan.innerHTML = uncheckedItems
+    })
+
+    // Create a list item to hold the checkbox, label and button
     const li = document.createElement('li')
     li.className = classNames.TODO_ITEM
     li.id = 'listItem' + numItems
     li.appendChild(checkBox)
     li.appendChild(checkBoxLabel)
-
-    const button = document.createElement('button')
-    button.innerHTML = 'delete'
-    button.className = classNames.TODO_DELETE + 'btn btn-danger btn-sm'
-    button.addEventListener('click', function(){
-      if (!document.getElementById('checkbox' + numItems).checked){
-        checkedItems--
-      }
-      document.getElementById('listItem' + numItems).remove()
-      numItems--
-      itemCountSpan.innerHTML = numItems
-      uncheckedCountSpan.innerHTML = checkedItems
-    })
-
     li.appendChild(button)
 
     list.appendChild(li)
     itemCountSpan.innerHTML = numItems
-    uncheckedCountSpan.innerHTML = checkedItems
-
+    uncheckedCountSpan.innerHTML = uncheckedItems
 
   }
-
-
 }
