950398accbd4ff6ee621ed693f70509d9363bfec
Raphael Uziel <raphaeluziel@hotmail.com>
2019-12-25 17:15:41 -0500

    works, but scope?

--- script.js
+++ script.js
@@ -9,37 +9,51 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let checkedItems = 0
+
 function newTodo() {
+
+  // Ask user for a new todo item by sending out a prompt
   let item = prompt("What should be added to the todo list?")
 
-  let numItems = parseInt(itemCountSpan.innerHTML)
-  numItems++
+  let numItems = itemCountSpan.innerHTML
 
-  let checkBox = document.createElement('input')
-  checkBox.type = 'checkbox'
-  checkBox.id = 'checkbox' + numItems
+  // Only if user writes a new todo, add it to the todo list
+  if (item){
 
-  let checkBoxLabel = document.createElement('label')
-  checkBoxLabel.htmlFor = 'checkbox' + numItems
+    numItems++
+    uncheckedCountSpan.innerHTML = numItems
 
-  let textNode = document.createTextNode(item)
+    let checkBox = document.createElement('input')
+    checkBox.type = 'checkbox'
+    checkBox.className = classNames.TODO_CHECKBOX
+    checkBox.id = 'checkbox' + numItems
+    checkBox.addEventListener('click', function(){
+      if (checkBox.checked){
+        checkedItems = parseInt(uncheckedCountSpan.innerHTML) - 1
+      }
+      else{
+        checkedItems = parseInt(uncheckedCountSpan.innerHTML) + 1
+      }
+      uncheckedCountSpan.innerHTML = checkedItems
+    })
 
-  checkBoxLabel.appendChild(textNode)
+    let checkBoxLabel = document.createElement('label')
+    checkBoxLabel.htmlFor = 'checkbox' + numItems
+    checkBoxLabel.className = classNames.TODO_TEXT
 
-  const li = document.createElement('li')
-  li.appendChild(checkBox)
-  li.appendChild(checkBoxLabel)
+    let textNode = document.createTextNode(item)
 
-  list.append(li)
-  itemCountSpan.innerHTML = numItems
+    checkBoxLabel.appendChild(textNode)
 
-  document.addEventListener('click', function(e){
-    console.log("POLICE", e.target.id)
-    //if (e.target && e.target.id === )
-  })
+    const li = document.createElement('li')
+    li.className = classNames.TODO_ITEM
+    li.appendChild(checkBox)
+    li.appendChild(checkBoxLabel)
 
-  for (let i = 0; i < numItems; i++){
-    //if document
+    list.append(li)
+    itemCountSpan.innerHTML = numItems
   }
+
 
 }
