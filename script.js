af11ba6d13ba097759f40f3a458213bf346552ba
raphaeluziel <raphaeluziel@hotmail.com>
2019-12-24 21:03:09 -0500

    first attempt

--- script.js
+++ script.js
@@ -10,5 +10,36 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  let item = prompt("What should be added to the todo list?")
+
+  let numItems = parseInt(itemCountSpan.innerHTML)
+  numItems++
+
+  let checkBox = document.createElement('input')
+  checkBox.type = 'checkbox'
+  checkBox.id = 'checkbox' + numItems
+
+  let checkBoxLabel = document.createElement('label')
+  checkBoxLabel.htmlFor = 'checkbox' + numItems
+
+  let textNode = document.createTextNode(item)
+
+  checkBoxLabel.appendChild(textNode)
+
+  const li = document.createElement('li')
+  li.appendChild(checkBox)
+  li.appendChild(checkBoxLabel)
+
+  list.append(li)
+  itemCountSpan.innerHTML = numItems
+
+  document.addEventListener('click', function(e){
+    console.log("POLICE", e.target.id)
+    //if (e.target && e.target.id === )
+  })
+
+  for (let i = 0; i < numItems; i++){
+    //if document
+  }
+
 }
